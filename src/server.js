a6fb4c9f680f98094bc4ac6115258cc38e350aaa
Moyses Aguagallo <leo.aigaje12@gmail.com>
2020-04-21 15:14:13 -0500

    Add CORS

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 //inicializacion del servidor
 const app =  express();
@@ -11,12 +12,17 @@ const app =  express();
 /*
 * Settings
 */
-app.set('port', process.env.PORT || 3000); //asignar puerto
+app.set('port', process.env.PORT || 5000); //asignar puerto
 
 /*
 * Middlewares
 */
 app.use(bodyParser.json()) //conversion a json peticiones POST
+/*
+//--> Habilitar el cosumo de mi API
+*/
+app.use(cors())
+
 
 /*
 * Routers
